7da13d22710f289ee58653bcb6384f4d6774485c
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-22 01:18:43 +0000

    [FiX]: conditional

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,11 +9,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 	
 	node = malloc(sizeof(binary_tree_t));
-	if (!node)
+	if (node == NULL)
 		return (NULL);
 
 	node = binary_tree_node(parent, value);
