18c9e73ee74f1ba98d3e1f99906c63e7820282a7
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-23 01:07:53 +0000

    [Fix]: minor issue

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t hl, hr;
 
 	if (!tree)
-		return (-1);
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
 
 	hl = binary_tree_height(tree->left) + 1;
 	hr = binary_tree_height(tree->right) + 1;
